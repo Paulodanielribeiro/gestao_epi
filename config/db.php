42c53cec7117095d60855a7c211c435bc02ff938
Paulodanielribeiro <paulomoraesr6@gmail.com>
2025-01-15 08:27:34 -0400

    upate

--- config/db.php
+++ config/db.php
@@ -1,11 +1,13 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$pass = '';
-$db = 'gestao_epi';
+$password = '';
+$dbname = 'gestao_epi';
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Conexão com o banco de dados
+$conn = new mysqli($host, $user, $password, $dbname);
 
+// Verificação de erro
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
